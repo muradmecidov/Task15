73c04c6e270507b0fadb8612f1ac5ad28a78ab9a
muradmecidov <muradmecidov1499@gmail.com>
2023-05-01 10:50:10 +0400

    Task14

---
+++ ConsoleApp12/Models/UrlsQuery.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12.Models
+{
+
+
+
+    internal class UrlsQuery
+    {
+
+
+        public string[] urls = new string[]
+        {
+
+        "https://tap.az/",
+        "https://autonet.az/",
+        "https://tap.az/",
+        "https://lalafo.az/",
+        "https://translate.google.com/",
+        "https://www.aztu.edu.az/az"
+        };
+
+
+
+
+
+
+        public void GetHttpContent(string[] url)
+        {
+            HttpClient httpClient = new HttpClient();
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            foreach (var itme in url)
+            {
+                var result = httpClient.GetStringAsync(itme).Result;
+                Console.WriteLine(result.Length);
+            }
+            stopwatch.Stop();
+            Console.WriteLine($"{stopwatch.ElapsedMilliseconds} ms");
+        }
+
+
+
+        public  async Task GetHttpContentAsync(string[] url)
+        {
+            HttpClient httpClient = new HttpClient();
+            List<Task<string>> list = new List<Task<string>>();
+            Stopwatch stopwatch = new Stopwatch();
+            
+            foreach (var itme in url)
+            {
+                var result = httpClient.GetStringAsync(itme);
+                list.Add(result);
+            }
+            stopwatch.Start();
+            string[] arr = await Task.WhenAll(list);
+
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item.Length);
+            }
+            stopwatch.Stop();
+            Console.WriteLine($"{stopwatch.ElapsedMilliseconds} ms");
+
+        }
+    }
+}
---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,17 @@
+﻿using ConsoleApp12.Models;
+
+namespace ConsoleApp12
+{
+    internal class Program
+    {
+        static async void Main(string[] args)
+        { 
+            UrlsQuery urlsQuery = new UrlsQuery();
+            urlsQuery.GetHttpContent(urlsQuery.urls);
+            urlsQuery.GetHttpContentAsync(urlsQuery.urls);
+
+
+
+        }
+    }
+}
